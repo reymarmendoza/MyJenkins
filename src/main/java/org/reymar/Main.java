21788afe7ab0374c247cc85505770fd72a1e236f
reymar <reymar.mendoza@gmail.com>
2023-05-21 16:19:42 -0500

    tres

--- src/main/java/org/reymar/Main.java
+++ src/main/java/org/reymar/Main.java
@@ -5,5 +5,13 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+
+		while (true) {
+			System.out.print("Ingrese el primer número: ");
+			double numero1 = scanner.nextDouble();
+
+			System.out.print("Ingrese el segundo número: ");
+			double numero2 = scanner.nextDouble();
+		}
 	}
 }
