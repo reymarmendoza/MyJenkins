b46ff64c70abc4cd3305595beb49f2d1a11cc97d
reymar <reymar.mendoza@gmail.com>
2023-05-21 16:19:54 -0500

    cuatro

--- src/main/java/org/reymar/Main.java
+++ src/main/java/org/reymar/Main.java
@@ -12,6 +12,18 @@ public class Main {
 
 			System.out.print("Ingrese el segundo número: ");
 			double numero2 = scanner.nextDouble();
+
+			System.out.println("Seleccione la operación:");
+			System.out.println("1. Suma");
+			System.out.println("2. Resta");
+			System.out.println("3. Multiplicación");
+			System.out.println("4. División");
+			System.out.println("5. Salir");
+
+			System.out.print("Opción: ");
+			int opcion = scanner.nextInt();
+
+			double resultado;
 		}
 	}
 }
